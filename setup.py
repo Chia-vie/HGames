f7bb5351fcb408c2ef65ea327210b6482d3b59e9
Christine Ackerl <36702096+Chia-vie@users.noreply.github.com>
2021-08-21 10:04:46 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+# read in required packages from requirements.txt
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+setuptools.setup(
+    name="HGames",
+    version="0.0.0",
+    author='',
+    author_email='hernsteincoders@gmail.com',
+    description='Games de developted during Code Camp HErnstein',
+    long_description='''This package is currently under development. Stay tuned for updates.''',
+    url="https://github.com/Chia-vie/mockingpy",
+    license='BSD',
+    keywords='astronomy, astrophysics, galaxies, spectra,...',
+    packages=setuptools.find_packages(),
+    package_data={'HGames': ['default_config.yml']},
+    classifiers=[
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',],
+    install_requires=requirements,
+    include_package_data=True
+)
